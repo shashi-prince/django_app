7ae8a136495422430b32c76ce14e7d20c133512b
Sashi Kanth Tamma <sashikanth.tamma@yahooinc.com>
2024-01-06 15:40:38 -0700

    add db changes

--- config/settings.py
+++ config/settings.py
@@ -225,7 +225,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db/db.sqlite3",
+        "NAME": BASE_DIR / "db/db_new.sqlite3",
     }
 }
 
